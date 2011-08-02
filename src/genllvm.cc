adb746013e965172266518ec3095f2e15660c54c
chz <chz@bingularity.org>
2011-08-02 08:56:32 -0700

    Don't generate memory base offset addition if base is zero.

--- src/genllvm.cc
+++ src/genllvm.cc
@@ -247,10 +247,12 @@ void GenLLVM::store(Value* addr_v, Value* data_v)
 			getGlobalContext(), sizeof(void*)*8));
 	}
 #endif
-	addr_v = builder->CreateAdd(addr_v, 
-		ConstantInt::get(getGlobalContext(),
-			APInt(sizeof(intptr_t)*8,
-			(uintptr_t)guest->getMem()->getBase())));
+	if (guest->getMem()->getBase()) {
+		addr_v = builder->CreateAdd(addr_v, 
+			ConstantInt::get(getGlobalContext(),
+				APInt(sizeof(intptr_t)*8,
+				(uintptr_t)guest->getMem()->getBase())));
+	}
 	addr_ptr = builder->CreateIntToPtr(addr_v, ptrTy, "storePtr");
 	si = builder->CreateStore(data_v, addr_ptr);
 	si->setAlignment(8);
@@ -270,10 +272,12 @@ Value* GenLLVM::load(Value* addr_v, const Type* ty)
 			getGlobalContext(), sizeof(void*)*8));
 	}
 #endif
-	addr_v = builder->CreateAdd(addr_v, 
-		ConstantInt::get(getGlobalContext(),
-			APInt(sizeof(intptr_t)*8,
-			(uintptr_t)guest->getMem()->getBase())));
+	if (guest->getMem()->getBase()) {
+		addr_v = builder->CreateAdd(addr_v, 
+			ConstantInt::get(getGlobalContext(),
+				APInt(sizeof(intptr_t)*8,
+				(uintptr_t)guest->getMem()->getBase())));
+	}
 	addr_ptr = builder->CreateIntToPtr(addr_v, ptrTy, "loadPtr");
 	loadInst = builder->CreateLoad(addr_ptr);
 	loadInst->setAlignment(8);
