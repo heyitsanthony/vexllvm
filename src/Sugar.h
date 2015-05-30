839e210d5d328d525f15235ea2982d02f71dad20
chz <chz@bingularity.org>
2015-05-30 15:18:13 -0700

    misc: change sugar headers
    there were conflicts with klee

--- src/Sugar.h
+++ src/Sugar.h
@@ -1,14 +1,12 @@
-#ifndef __SUGAR__
-#define __SUGAR__
+#ifndef __SUGAR2__
+#define __SUGAR2__
 
 #include <cstring>
 
-// Lame C++ sugar.  I'm pretty sure there better definitions.
-
-#define let(_lhs, _rhs)  auto _lhs = _rhs
-
+#ifndef foreach
 #define foreach(_i, _b, _e) \
 	  for(auto _i = _b, _i ## end = _e; _i != _i ## end;  ++ _i)
+#endif
 
 #if 	defined(__has_feature) &&		\
 	__has_feature(address_sanitizer) &&	\
@@ -18,21 +16,11 @@
 #define ATTRIBUTE_NO_SANITIZE_ADDRESS
 #endif
 
-
-struct ltstr
-{
-    bool operator()(const char* str1, const char* str2) const
-    {
-        return std::strcmp(str1, str2) < 0;
-    }
-};
-
 #include <vector>
 #include <list>
 #include <memory>
 template<typename T> using ptr_vec_t = std::vector<std::unique_ptr<T>>;
 template<typename T> using ptr_list_t = std::list<std::unique_ptr<T>>;
 
-
-#endif /* __SUGAR__ */
+#endif /* __SUGAR2__ */
 
